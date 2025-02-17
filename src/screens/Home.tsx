9201bdad52f78f533f818c5b34b2c9ffd6f5f68f
Fazekas Milán <fazekas.milan1@gmail.com>
2025-02-17 19:20:05 +0100

    change favicon and team to svg

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -76,7 +76,7 @@ export default function Home() {
                                 navigate('/edit', { state: { team } })
                             }}
                         />
-                        <img src="https://scontent-vie1-1.xx.fbcdn.net/v/t1.15752-9/416385981_1160666734911714_6105998791939796377_n.png?stp=dst-png_s100x100&_nc_cat=109&ccb=1-7&_nc_sid=b70caf&_nc_ohc=Jyq0yw3zbuMQ7kNvgHgKjDm&_nc_oc=AdgCwKSOndTAzyV9XqHWlc_2anGBanz8ro4ak9hTJakZUd8iMH9JKS3FDQbcpaKvP-MM9i6npvQsSvpzCP0_v85j&_nc_zt=23&_nc_ht=scontent-vie1-1.xx&oh=03_Q7cD1gGVNFoqOcUoQHWBSMrZLvY2c85InGb_lNLJ8-U7mEJshA&oe=67D19D71" alt="Csapatlogó" />
+                        <img src="/public/out.svg" alt="Csapatlogó" />
                         <span>{team.name}</span>
                     </div>
                 ))}
