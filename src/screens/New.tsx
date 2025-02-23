883c91c970f6132bf4ff50523ce30f323615705d
Fazekas Milán <fazekas.milan1@gmail.com>
2025-02-23 20:42:08 +0100

    goals and assists table

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -10,17 +10,39 @@ export default function New() {
     const { state } = useLocation();
     const navigate = useNavigate();
 
-    const [team, _] = useState<{ id: string, name: string, players: {name: string, number: number}[] }>(state.team);
+    const [team, _] = useState<{ id: string, name: string, players: { name: string, number: number }[] }>(state.team);
 
     const [opponentName, setOpponentName] = useState<string>('');
 
-    const [savedStats, setSavedStats] = useState<{ modified: number, id: string, teamId: string, opponentName: string, scorers: { name: string, number: number, isOurScore: boolean }[] }[]>([]);
+    const [savedStats, setSavedStats] = useState<{ modified: number, id: string, teamId: string, opponentName: string, scorers: { assist: number, goal: number, isOurScore: boolean }[] }[]>([]);
+
+    const [assistDic, setAssistDic] = useState<{ [key: string]: number }>({});
+    const [goalDic, setGoalDic] = useState<{ [key: string]: number }>({});
 
     useEffect(() => {
-        const storageStats: { modified: number, id: string, teamId: string, opponentName: string, scorers: { name: string, number: number, isOurScore: boolean }[] }[] = JSON.parse(localStorage.getItem('stats') ?? '[]');
+        const storageStats: { modified: number, id: string, teamId: string, opponentName: string, scorers: { assist: number, goal: number, isOurScore: boolean }[] }[] = JSON.parse(localStorage.getItem('stats') ?? '[]');
         const filtered = storageStats.filter(s => s.teamId === team.id);
         setSavedStats(filtered);
+
     }, []);
+
+    useEffect(() => {
+        let ad: { [key: number]: number } = {};
+        let gd: { [key: number]: number } = {};
+        savedStats.forEach(s => {
+            s.scorers.forEach(sc => {
+                if (sc.assist === -1 || sc.goal === -1) return;
+                if (sc.assist === sc.goal) {
+                    gd[sc.goal] ? gd[sc.goal]++ : gd[sc.goal] = 1;
+                    return;
+                }
+                ad[sc.assist] ? ad[sc.assist]++ : ad[sc.assist] = 1;
+                gd[sc.goal] ? gd[sc.goal]++ : gd[sc.goal] = 1;
+            });
+        });
+        setAssistDic(ad);
+        setGoalDic(gd);
+    }, [savedStats])
 
     return (
         <div
@@ -28,35 +50,35 @@ export default function New() {
         >
             <h1>{team.name}</h1>
             <h2>Játékosok</h2>
-            {team.players.sort((a, b) => a.number - b.number).map((player, index)=> (
+            {team.players.sort((a, b) => a.number - b.number).map((player, index) => (
                 <div
-		    key={index}
+                    key={index}
                     className={`${styles.player} player`}
                 >
-		    <span>{player.number}</span>
-		    <span>{player.name}</span>
+                    <span>{player.number}</span>
+                    <span>{player.name}</span>
                 </div>
             ))}
-	    <h2>Ellenfél</h2>
-	    <form
-		onSubmit={(e) => {
-		    e.preventDefault();
-		    if (e.currentTarget.checkValidity()) navigate('/stats', { state: { ...state, opponentName: opponentName ?? '', id: uid() } });
-		    
-		    else e.currentTarget.reportValidity();
-		}}
-	    >
-		<input
-		    required
-		    type="text" 
-		    placeholder='Csapatnév'
-		    onChange={(e) => setOpponentName(e.target.value)}
-		/>
-		<br />
-		<button
-		    className='button'
-		>Új jegyzőkönyv indítása</button>
-	    </form>
+            <h2>Ellenfél</h2>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    if (e.currentTarget.checkValidity()) navigate('/stats', { state: { ...state, opponentName: opponentName ?? '', id: uid() } });
+
+                    else e.currentTarget.reportValidity();
+                }}
+            >
+                <input
+                    required
+                    type="text"
+                    placeholder='Csapatnév'
+                    onChange={(e) => setOpponentName(e.target.value)}
+                />
+                <br />
+                <button
+                    className='button'
+                >Új jegyzőkönyv indítása</button>
+            </form>
             {savedStats.length > 0 &&
                 <>
                     <h2
@@ -65,9 +87,9 @@ export default function New() {
                     <div
                         className={styles.savedStatsContainer}
                     >
-                        {savedStats.sort((a, b) => b.modified - a.modified).map((stat, index)=> (
+                        {savedStats.sort((a, b) => b.modified - a.modified).map((stat, index) => (
                             <div
-				key={index}
+                                key={index}
                                 className={styles.stat}
                                 onClick={() => {
                                     navigate('/stats', { state: { ...state, opponentName: stat.opponentName, id: stat.id } });
@@ -86,10 +108,10 @@ export default function New() {
                                     icon={faTrash}
                                     onClick={(e) => {
                                         e.stopPropagation();
-                                        let stats: { modified: number, id: string, teamId: string, opponentName: string, scorers: { name: string, number: number, isOurScore: boolean }[] }[] = JSON.parse(localStorage.getItem('stats') ?? '[]');
+                                        let stats: { modified: number, id: string, teamId: string, opponentName: string, scorers: { assist: number, goal: number, isOurScore: boolean }[] }[] = JSON.parse(localStorage.getItem('stats') ?? '[]');
                                         stats = stats.filter(s => s.id !== stat.id);
                                         localStorage.setItem('stats', JSON.stringify(stats));
-					stats = stats.filter(s => s.teamId === team.id);
+                                        stats = stats.filter(s => s.teamId === team.id);
                                         setSavedStats(stats);
                                     }}
                                 />
@@ -99,6 +121,44 @@ export default function New() {
                             </div>
                         ))}
                     </div>
+                    <h2>Kanadai táblázat</h2>
+                    <table
+                        className={styles.table}
+                    >
+                        <thead>
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>
+                                    Assziszt
+                                </th>
+                                <th>
+                                    Pont
+                                </th>
+                                <th>
+                                    Összesen
+                                </th>
+                            </tr>
+                        </thead>
+                        {
+                            <tbody>
+                                {
+                                    team.players.sort((a, b) => a.number - b.number).map((p, index) => (
+                                        <tr
+                                            key={index}
+                                        >
+                                            <td>{p.number}</td>
+                                            <td>{assistDic[p.number] ?? 0}</td>
+                                            <td>{goalDic[p.number] ?? 0}</td>
+                                            <td>{(assistDic[p.number] ?? 0) + (goalDic[p.number] ?? 0)}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+
+                        }
+                    </table>
                 </>
             }
         </div >
