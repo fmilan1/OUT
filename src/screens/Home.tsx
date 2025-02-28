252b1a77a71a423068869c3d35204bed7cbed518
Fazekas Milán <fazekas.milan1@gmail.com>
2025-02-28 21:13:29 +0100

    fix players shown when new team is being added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -116,6 +116,7 @@ export default function Home() {
                                     state: {
                                         team,
                                         userId,
+                                        players,
                                     }
                                 });
                             }}
