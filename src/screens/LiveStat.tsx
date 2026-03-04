308aef514a90676559c8b0cc43633132a193b24f
Fazekas Milán <fazekas.milan1@gmail.com>
2026-03-04 19:11:09 +0100

    fix

--- src/screens/LiveStat.tsx
+++ src/screens/LiveStat.tsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react"
 import { db } from "../firebase";
 import styles from '../styles/LiveStat.module.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightLong, faCaretLeft, faChevronLeft, faCross, faCrosshairs, faSkullCrossbones, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { Player } from "./Home";
+import { useNavigate } from "react-router";
 
 export default function LiveStat() {
     const [id, _setId] = useState(window.location.search.replace('?', ''));
+    const [teamId, _setTeamId] = useState('');
     const [scorers, setScorers] = useState<{ assist: number, goal: number, isOurScore: boolean }[]>([]);
     const [ended, setEnded] = useState(false);
     const [isGirlRatio, setIsGirlRatio] = useState();
@@ -24,6 +26,7 @@ export default function LiveStat() {
                     if (stat.ref.parent.parent) {
                         const team = await getDoc(stat.ref.parent.parent);
                         setTeamName(team.get('name'));
+                        _setTeamId(team.id);
                         const players = collection(db, team.ref.path, 'players');
                         const playersSnap = await getDocs(players);
                         const playersData = playersSnap.docs.map(p => ({
@@ -47,76 +50,93 @@ export default function LiveStat() {
         fetch();
     }, []);
 
+    const navigate = useNavigate();
     return (
-        <div
-            className={styles.container}
-        >
+        <>
             <div
-                className={styles.innerContainer}
+                className={styles.crossBtn}
+                onClick={() => {
+                    navigate(`/live?${teamId}`)
+                }}
+            >
+                <FontAwesomeIcon
+                    icon={faChevronLeft}
+                />
+            </div>
+            <div
+                className={styles.container}
             >
                 <div
-                    className={styles.scoreContainer}
+                    className={styles.innerContainer}
                 >
-                    <span>{teamName}</span>
                     <div
-                        className={styles.scores}
+                        className={styles.scoreContainer}
                     >
-                        <h1>{scorers.filter(sc => sc.isOurScore).length}</h1>
-                        <h1>:</h1>
-                        <h1>{scorers.filter(sc => !sc.isOurScore).length}</h1>
-                    </div>
-                    <span>{opponentName}</span>
-                    {!ended &&
-                        <span
-                            className={styles.ratio}
-                        >
-                            {isGirlRatio ? 'Lány pont' : 'Fiú pont'}
-                        </span>
-                    }
-                    <span
-                        className={styles.status}
-                    >
-                        {ended ? 'Vége' : 'Élő'}
-                    </span>
-                </div>
-                <div
-                    className={styles.rows}
-                >
-                    {scorers.map((sc, index) => (
+                        <span>{teamName}</span>
                         <div
-                            key={index}
-                            className={styles.row}
+                            className={styles.scores}
                         >
-                            {sc.isOurScore &&
-                                <div
-                                    className={styles.our}
-                                >
-                                    <div>#{sc.assist} {players.find(p => p.number === sc.assist)?.name}</div>
-                                    <FontAwesomeIcon
-                                        icon={faArrowRightLong}
-                                    />
-                                    <div>#{sc.goal} {players.find(p => p.number === sc.goal)?.name}</div>
-                                </div>
-                            }
-                            <div
-                                className={styles.actualScore}
-                            >
-                                <span>{scorers.filter((s, idx) => s.isOurScore && index >= idx).length}</span>
-                                <span> : </span>
-                                <span>{scorers.filter((s, idx) => !s.isOurScore && index >= idx).length}</span>
-                            </div>
-                            {!sc.isOurScore &&
-                                <div
-                                    className={styles.opponent}
-                                >
-                                    <span>Ellenfél szerzett pontot :(</span>
-                                </div>
-                            }
+                            <h1>{scorers.filter(sc => sc.isOurScore).length}</h1>
+                            <h1>:</h1>
+                            <h1>{scorers.filter(sc => !sc.isOurScore).length}</h1>
                         </div>
-                    ))}
+                        <span>{opponentName}</span>
+                        {!ended &&
+                            <span
+                                className={styles.ratio}
+                            >
+                                {isGirlRatio ? 'Lány pont' : 'Fiú pont'}
+                            </span>
+                        }
+                        <span
+                            className={styles.status}
+                        >
+                            {ended ? 'Vége' : 'Élő'}
+                        </span>
+                    </div>
+                    <div
+                        className={styles.rows}
+                    >
+                        {scorers.map((sc, index) => (
+                            <div
+                                key={index}
+                                className={styles.row}
+                            >
+                                {sc.isOurScore &&
+                                    <div
+                                        className={styles.our}
+                                    >
+                                        {sc.assist !== sc.goal ?
+                                            <div>#{sc.assist} {players.find(p => p.number === sc.assist)?.name}</div>
+                                            :
+                                            <div>CALLAHAN</div>
+                                        }
+                                        <FontAwesomeIcon
+                                            icon={faArrowRightLong}
+                                        />
+                                        <div>#{sc.goal} {players.find(p => p.number === sc.goal)?.name}</div>
+                                    </div>
+                                }
+                                <div
+                                    className={styles.actualScore}
+                                >
+                                    <span>{scorers.filter((s, idx) => s.isOurScore && index >= idx).length}</span>
+                                    <span> : </span>
+                                    <span>{scorers.filter((s, idx) => !s.isOurScore && index >= idx).length}</span>
+                                </div>
+                                {!sc.isOurScore &&
+                                    <div
+                                        className={styles.opponent}
+                                    >
+                                        <span>Ellenfél szerzett pontot :(</span>
+                                    </div>
+                                }
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
